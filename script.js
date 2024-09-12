f96e17f85e83a1de11c424e6c831afcc6f690328
Md. Nesad <131846011+Md-Nesad@users.noreply.github.com>
2024-09-12 13:41:50 +0600

    Landing page first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// sliding on click on anchor tag
+let slide = document.querySelectorAll('a');
+    slide.forEach(anchor => {
+        anchor.addEventListener('click', function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+        });
+});
+// scroll to top function
+window.onscroll = function() {
+    let scrollBtn = document.getElementById('scroll-Btn');
+    if(document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+        scrollBtn.style.display = 'block';
+    }else{
+        scrollBtn.style.display = 'none';
+    }
+}
+
+function scrollToTop () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    alert('Back to top')
+}
+// resposive nav
+function showNav() {
+    let nav = document.getElementById('navigation');
+    let button = document.getElementById('res-button');
+     if(nav.style.display === 'block') {
+        nav.style.display = 'none';
+        button.innerHTML = '<i class="ri-menu-line"></i>'
+     }else{
+        nav.style.display = 'block';
+        button.innerHTML = '<i class="ri-close-large-line"></i>'
+     }
+}
